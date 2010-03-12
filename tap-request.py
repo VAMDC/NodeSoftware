d39526827e8097bf4e8ee9e975fcf12b98b02685
Thomas Marquart <thomas@marquart.se>
2010-03-12 10:01:17 +0100

    rewrite tap-request, make it into a class

--- tap-request.py
+++ tap-request.py
@@ -16,10 +16,11 @@ Not yet working: read the result into ATPy's implementation
 of VOTable.
 
 """
-import urllib
+# don't use a proxy
 from os import environ
 environ["http_proxy"]=''
-import sgmllib
+
+import urllib
 from lxml import etree as e
 from lxml.etree import XPath
 text_list=XPath("//text()")
@@ -30,7 +31,7 @@ from time import sleep
 from StringIO import StringIO
 import threading
 
-# XML schema
+# XML schemata
 xsl_job2html_url='http://vamdc.tmy.se:8080/DSAcat/TAP/uws-job-to-html.xsl'
 xsl_job2html = e.parse(xsl_job2html_url)
 job2html= e.XSLT(xsl_job2html)
@@ -43,68 +44,84 @@ xsl_xsams_url='http://vamdc.tmy.se:8080/DSAcat/xsams-0.1.xsd'
 xsl_xsams= e.parse(xsl_xsams_url)
 
 
-# where exactly to query
-url="http://vamdc.tmy.se:8080/DSAcat/TAP/async"
+valdurl="http://vamdc.tmy.se:8080/DSAcat/TAP/async"
+valdquery='SELECT TOP 5 VALD.merged.wavel,VALD.merged.ion FROM VALD.merged'
 
-# the query
-quer={}
-quer['LANG']='ADQL'
-quer['FORMAT']='application/x-votable+xml'
-quer['VERSION']='1.0'
-quer['QUERY']='SELECT TOP 5 VALD.merged.wavel,VALD.merged.ion FROM VALD.merged'
+class TAP(object):
+    def __init__(self,requrl=valdurl,query=valdquery):
+        self.requrl=requrl
+        quer={}
+        quer['LANG']='ADQL'
+        quer['FORMAT']='application/x-votable+xml'
+        quer['VERSION']='1.0'
+        quer['QUERY']=query
+        self.query=urllib.urlencode(quer)
 
-#
-#  Helper functions
-#
-def runquery(quer):
-    quer=urllib.urlencode(quer)
-    response = urllib.urlopen(url,quer) # making the actual request
-    xml_res=e.parse(response)
-    response.close()
-    # old way to find out the JobId:
-    #for el in xml_res.iter():
-    #    if 'jobId' in el.tag:
-    #        jobid=el.text
-    jobid=text_list(xml_res)[1] 
-    postdata=urllib.urlencode({'PHASE':'RUN'})
-    urllib.urlopen(url+'/'+jobid+'/phase',postdata)
+    def __repr__(self):
+        s=''
+        if self.result_xml:
+            for element in self.result_xml.iter():
+                if 'TR' in element.tag: s+='\n'
+                if 'TD' in element.tag: s+=element.text+'\t'
+        else:
+            s+="Not data here yet!"
+        return s
 
-    return jobid
+    def run(self):
+        self.send_query()
+        self.run_job()
+        self.fetch_result()
 
-def fetchresult(jobid):
-    # check if result is there
-    while urllib.urlopen(url+'/'+jobid+'/phase').read() != 'COMPLETED':
-        print 'waiting...'
-        sleep(1)
+    def send_query(self):
+        response = urllib.urlopen(self.requrl,self.query)
+        self.job_xml=e.parse(response)
+        response.close()
+        # old way to find out the JobId:
+        #for el in xml_res.iter():
+        #    if 'jobId' in el.tag:
+        #        jobid=el.text
+        self.jobid=text_list(self.job_xml)[1] 
 
-    resulturl=url+'/'+jobid+'/results/result'
-    data=urllib.urlopen(resulturl).read()
-    return data
+    def run_job(self):
+        postdata=urllib.urlencode({'PHASE':'RUN'})
+        urllib.urlopen(self.requrl+'/'+self.jobid+'/phase',postdata)
 
-def readVO(vodata):
-    t=atpy.Table()
-    t.read(StringIO(vodata),type='vo')
 
-    print t.wavel[2]
-    print t.columns
+    def fetch_result(self):
+        # check if result is there
+        while urllib.urlopen(self.requrl+'/'+self.jobid+'/phase').read() != 'COMPLETED':
+            print 'waiting...'
+            sleep(1)
+
+        resulturl=self.requrl+'/'+self.jobid+'/results/result'
+        res=urllib.urlopen(resulturl)
+        self.result_xml=e.parse(res,parser)
+        res.close()
+
+    def readVO(vodata):
+        t=atpy.Table()
+        t.read(StringIO(self.vodata),type='vo')
+        print t.wavel[2]
+        print t.columns
+    #open('data.vo','w').write(vodata)
+    #t=readVO(vodata) # this does not yet work yet since the vald-metadata are incomplete
 
 
 
 def main():
-    jobid=runquery(quer)
-    vodata=fetchresult(jobid)
-    vo=e.parse(StringIO(vodata),parser)
+    
     for el in vo.iter():
         if 'TR' in el.tag: print
         if 'TD' in el.tag: print el.text,
-    #open('data.vo','w').write(vodata)
-    #t=readVO(vodata) # this does not yet work yet since the vald-metadata are incomplete
 
 class MyThread ( threading.Thread ):
-    def run ( self ): main()
+    def run ( self ):
+        tap=TAP()
+        tap.run()
+        print tap
 
 
-if __name__=='__main__': main()
-#    for i in xrange(20):
-#        MyThread().start()
+if __name__=='__main__': 
+    for i in xrange(1):
+        MyThread().start()
 
