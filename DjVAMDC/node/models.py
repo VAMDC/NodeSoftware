2a253eca44b305537b004e84468c04604f3b7447
Thomas Marquart <thomas@marquart.se>
2010-04-19 15:55:11 +0200

    starting with db models, mimicking xsams

--- DjVAMDC/node/models.py
+++ DjVAMDC/node/models.py
@@ -19,37 +19,77 @@ class FloatFieldN(models.FloatField):
 class IntegerFieldN(models.IntegerField):
     __init__=allownull__init__
 
-
+#################################################################
 
 class Source(models.Model):
     class Meta:
         verbose_name = _('Source')
         verbose_name_plural = _('Sources')
+    sourceID=models.CharField(max_length=64,primary_key=True)
+    #Category=
+    #Year=
+    #SourceName=
+    #Volume=
+    #PageBegin=
+    
+class Author(models.Model):
+    class Meta:
+        verbose_name = _('Source')
+        verbose_name_plural = _('Sources')
+    Source=models.ForeignKey(Source,related_name='Authors')
+    Name=models.CharField(max_length=64)
+
+################################################################
 
 class Method(models.Model):
     class Meta:
         verbose_name = _('Method')
         verbose_name_plural = _('Methods')
+    methodID=models.CharField(max_length=64,primary_key=True)
+    #Category=
+    #Description=
+ 
 
-class State(models.Model):
+################################################################
+
+class AtomicState(models.Model):
     class Meta:
-        verbose_name = _('State')
-        verbose_name_plural = _('States')
+        verbose_name = _('AtomicState')
+        verbose_name_plural = _('AtomicStates')
+    stateID=models.CharField(max_length=64,primary_key=True)
+    Description=models.CharField(max_length=64)
+    
 
+class Isotope(models.Model):
+    class Meta:
+        verbose_name = _('Isotope')
+        verbose_name_plural = _('Isotopes')
+    
 class Atom(models.Model):
     class Meta:
         verbose_name = _('Atom')
         verbose_name_plural = _('Atoms')
+    AtomicState=models.ForeignKey(AtomicState)
+
+################################################################
 
 class Molecule(models.Model):
     class Meta:
         verbose_name = _('Molecule')
         verbose_name_plural = _('Molecules')
+    MolecularState=models.ForeignKey(MolecularState)
 
+class MolecularState(models.Model)
+    class Meta:
+        verbose_name = _('Molecule')
+        verbose_name_plural = _('Molecules')
+    
 class Particle(models.Model):
     class Meta:
         verbose_name = _('Particle')
         verbose_name_plural = _('Particle')
+
+###############################################################
 
 class Process(models.Model):
     class Meta:
--- DjVAMDC/vald/models.py
+++ DjVAMDC/vald/models.py
@@ -1,3 +1,6 @@
 from DjVAMDC.node.models import *
 
 ## where VALD deviates from the standard node model goes here!
+
+#class Transition(Process):
+    
