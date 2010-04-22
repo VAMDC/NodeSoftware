4b34ec45c863e2c9b2bfa84bd33ed058e0cece2b
Thomas Marquart <thomas@marquart.se>
2010-04-22 12:12:09 +0200

    moving some thigns around

--- DjVAMDC/tapservice/views.py
+++ DjVAMDC/tapservice/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render_to_response,get_object_or_404
 from django.template import RequestContext, Context, loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 
 from DjVAMDC.vald.models import Transition,State,Source,Species
+from DjVAMDC.vald.views import *
 
-from django.db.models import Q
 
 from time import time
 from datetime import date
@@ -24,10 +25,10 @@ def index(request):
 class TAPQUERY(object):
     def __init__(self,data):
         try:
-            self.request=data['REQUEST']
-            self.lang=data['LANG']
-            self.query=data['QUERY']
-            self.format=data['FORMAT']
+            self.request=lower(data['REQUEST'])
+            self.lang=lower(data['LANG'])
+            self.query=lower(data['QUERY'])
+            self.format=lower(data['FORMAT'])
             self.isvalid=True
         except:
             self.isvalid=False
@@ -48,9 +49,9 @@ class TAPQUERY(object):
         return '%s'%self.query
 
 def parseSQL(sql):
-    wheres=sql.split('WHERE')[1].split('AND') # replace this with http://code.google.com/p/python-sqlparse/ later
+    wheres=sql.split('where')[1].split('and') # replace this with http://code.google.com/p/python-sqlparse/ later
     qlist=[]
-    for w in map(lower,wheres):
+    for w in wheres:
         w=w.split()
         print w
         value=w[-1]
@@ -67,77 +68,6 @@ def parseSQL(sql):
     return tuple(qlist)
             
 
-def getVALDsources1(transs):
-    # this is REALLY slow
-    waverefs=Q(iswaveref_trans__in=transs)
-    landerefs=Q(islanderef_trans__in=transs)
-    loggfrefs=Q(isloggfref_trans__in=transs)
-    g1refs=Q(isgammaradref_trans__in=transs)
-    g2refs=Q(isgammastarkref_trans__in=transs)
-    g3refs=Q(isgammawaalsref_trans__in=transs)
-    refQ= waverefs | landerefs | loggfrefs | g1refs | g2refs | g3refs
-    sources=Source.objects.filter(refQ).distinct()
-    return sources
-
-def getVALDsources2(transs):
-    waverefs=Q(iswaveref_trans__in=transs)
-    landerefs=Q(islanderef_trans__in=transs)
-    loggfrefs=Q(isloggfref_trans__in=transs)
-    g1refs=Q(isgammaradref_trans__in=transs)
-    g2refs=Q(isgammastarkref_trans__in=transs)
-    g3refs=Q(isgammawaalsref_trans__in=transs)
-    refQs=[waverefs, landerefs, loggfrefs, g1refs, g2refs, g3refs]
-    sources=set()
-    for q in refQs:
-        refs=Source.objects.filter(q)
-        for r in refs:
-            sources.add(r)
-    return sources
-
-def getVALDsources3(transs):
-    sids=set([])
-    for trans in transs:
-        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
-        sids=sids.union(s)
-    return list(sids)
-
-def getVALDsources4(transs):
-    sids=set([])
-    for trans in transs:
-        s=set([trans.wave_ref.id,trans.loggf_ref.id,trans.lande_ref.id,trans.gammarad_ref.id,trans.gammastark_ref.id,trans.gammawaals_ref.id])
-        sids=sids.union(s)
-    sources=[]
-    for sid in sids:
-        sources.append(Source.objects.get(pk=sid))
-    return sources
-
-def getVALDsources5(transs):
-    sids=set([])
-    for trans in transs:
-        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
-        sids=sids.union(s)
-    sources=[]
-    for sid in sids:
-        sources.append(Source.objects.get(pk=sid))
-    return sources
-
-def getVALDstates1(transs):
-    lostates=State.objects.filter(islowerstate_trans__in=transs)
-    histates=State.objects.filter(islowerstate_trans__in=transs)
-    states = lostates | histates
-    return states.distinct()
-    
-
-def getVALDstates2(transs):
-    sids=set([])
-    for trans in transs:
-        s=set([trans.lostate,trans.upstate])
-        sids=sids.union(s)
-    states=[]
-    for sid in sids:
-        states.append(State.objects.get(pk=sid))
-    return states
-
 class RenderThread(threading.Thread):
     def __init__(self, t,c):
         threading.Thread.__init__(self)
@@ -146,18 +76,6 @@ class RenderThread(threading.Thread):
     def run(self):
         self.r=self.t.render(self.c)
         
-
-def MyRender(format,transs,states,sources):
-    if format.lower()=='xsams':
-        rend=RenderXSAMSmulti(format,transs,states,sources)
-    elif format.lower()=='csv': 
-        t=loader.get_template('vald/valdtable.csv')
-        c=Context({'transitions':transs,'sources':sources,'states':states,})
-        rend=t.render(c)
-    else: 
-        rend=''   
-    return rend
-
 
 def RenderXSAMSmulti(format,transs,states,sources):
     n=int(transs.count()/3)
@@ -190,6 +108,19 @@ def RenderXSAMSsingle(format,transs,states,sources):
     rend=r1+r2+ u'\n</XSAMSData>'
     return rend
 
+
+def MyRender(format,transs,states,sources):
+    if format=='xsams':
+        rend=RenderXSAMSsingle(format,transs,states,sources)
+    elif format=='csv': 
+        t=loader.get_template('vald/valdtable.csv')
+        c=Context({'transitions':transs,'sources':sources,'states':states,})
+        rend=t.render(c)
+    else: 
+        rend=''   
+    return rend
+
+
 def sync(request):
     tap=TAPQUERY(request.REQUEST)
     if not tap.isvalid:
@@ -200,7 +131,7 @@ def sync(request):
     
     ts=time()
 
-    transs=Transition.objects.filter(*qtup)
+    transs=Transition.objects.filter(*qtup).order_by('vacwave')
     #print '%d transitions set up'%len(transs),time()-ts
     
     states = getVALDstates2(transs)
@@ -222,7 +153,7 @@ def sync(request):
     response = HttpResponse(compressed_content,mimetype='application/x-gzip')
     response['Content-Encoding'] = 'gzip'
     response['Content-Length'] = str(len(compressed_content))
-    response['Content-Disposition'] = 'attachment; filename=%s%s.gz'%(tap.queryid,tap.format)
+    response['Content-Disposition'] = 'attachment; filename=%s.%s.gz'%(tap.queryid,tap.format)
     return response
         #return render_to_response('vald/valdxsams.xml', c) # shortcut
 
@@ -241,16 +172,3 @@ def tables(request):
 def availability(request):
     c=RequestContext(request,{})
     return render_to_response('node/index.html', c)
-
-
-def compressedview(request):
-    zbuf = cStringIO.StringIO()
-    zfile = gzip.GzipFile(mode='wb', compresslevel=6, fileobj=zbuf)
-    zfile.write(template.render(context).encode('utf-8'))
-    zfile.close()
-    
-    compressed_content = zbuf.getvalue()
-    response = HttpResponse(compressed_content)
-    response['Content-Encoding'] = 'gzip'
-    response['Content-Length'] = str(len(compressed_content))
-    return response
--- DjVAMDC/vald/views.py
+++ DjVAMDC/vald/views.py
@@ -1,6 +1,78 @@
 from DjVAMDC.node.views import *
+from DjVAMDC.vald.models import Transition,State,Source,Species
 import cStringIO, gzip
 
+
+def getVALDsources1(transs):
+    # this is REALLY slow
+    waverefs=Q(iswaveref_trans__in=transs)
+    landerefs=Q(islanderef_trans__in=transs)
+    loggfrefs=Q(isloggfref_trans__in=transs)
+    g1refs=Q(isgammaradref_trans__in=transs)
+    g2refs=Q(isgammastarkref_trans__in=transs)
+    g3refs=Q(isgammawaalsref_trans__in=transs)
+    refQ= waverefs | landerefs | loggfrefs | g1refs | g2refs | g3refs
+    sources=Source.objects.filter(refQ).distinct()
+    return sources
+
+def getVALDsources2(transs):
+    waverefs=Q(iswaveref_trans__in=transs)
+    landerefs=Q(islanderef_trans__in=transs)
+    loggfrefs=Q(isloggfref_trans__in=transs)
+    g1refs=Q(isgammaradref_trans__in=transs)
+    g2refs=Q(isgammastarkref_trans__in=transs)
+    g3refs=Q(isgammawaalsref_trans__in=transs)
+    refQs=[waverefs, landerefs, loggfrefs, g1refs, g2refs, g3refs]
+    sources=set()
+    for q in refQs:
+        refs=Source.objects.filter(q)
+        for r in refs:
+            sources.add(r)
+    return sources
+
+def getVALDsources3(transs):
+    sids=set([])
+    for trans in transs:
+        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
+        sids=sids.union(s)
+    return list(sids)
+
+def getVALDsources4(transs):
+    sids=set([])
+    for trans in transs:
+        s=set([trans.wave_ref.id,trans.loggf_ref.id,trans.lande_ref.id,trans.gammarad_ref.id,trans.gammastark_ref.id,trans.gammawaals_ref.id])
+        sids=sids.union(s)
+    sources=[]
+    for sid in sids:
+        sources.append(Source.objects.get(pk=sid))
+    return sources
+
+def getVALDsources5(transs):
+    sids=set([])
+    for trans in transs:
+        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
+        sids=sids.union(s)
+    sources=[]
+    for sid in sids:
+        sources.append(Source.objects.get(pk=sid))
+    return sources
+
+def getVALDstates1(transs):
+    lostates=State.objects.filter(islowerstate_trans__in=transs)
+    histates=State.objects.filter(islowerstate_trans__in=transs)
+    states = lostates | histates
+    return states.distinct()
+    
+
+def getVALDstates2(transs):
+    sids=set([])
+    for trans in transs:
+        s=set([trans.lostate,trans.upstate])
+        sids=sids.union(s)
+    states=[]
+    for sid in sids:
+        states.append(State.objects.get(pk=sid))
+    return states
 
 def compressedview(request):
     zbuf = cStringIO.StringIO()
