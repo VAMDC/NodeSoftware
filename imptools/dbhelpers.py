51bb8e76511edac0e65446a24bf88feaf5be978f
Griatch (Samuel R) <samreg@astro.uu.se>
2010-06-04 23:22:33 +0800

    Some minor cleanup and commenting on the codes.

--- imptools/dbhelpers.py
+++ imptools/dbhelpers.py
@@ -17,10 +17,10 @@ def handler():
     except Exception, e:
         print e
 
-import string as s
+#import string as s
 import sys
 
-from django.db.utils import IntegrityError
+#from django.db.utils import IntegrityError
 from django.db.models import Q
 
 def readcfg(fname):
@@ -34,28 +34,44 @@ def readcfg(fname):
     exec(f.read())
     return config
 
-def getdata(tconf,line,model):
+def process_line(line, colconf):
+    "Process one line of data"
+    colfunc = colconf['cbyte'][0]
+    args = colconf['cbyte'][1]
+    dat = colfunc(line, *args)
+    if not dat or colconf.has_key('cnull') \
+           and dat == colconf['cnull']:
+        return None
+    return dat
+
+def getdata(tconf, line, model):
+    """
+    Get data, alternatively create new model
+    """
     if tconf.has_key('updatematch'):
-        colconf=tconf['columns'][0]
-        fu=colconf['cbyte'][0]
-        args=colconf['cbyte'][1]
-        d=fu(line,*args)
-        if not d: return None
-        exec('modelq=Q(%s="%s")'%(tconf['updatematch'],d))
+        dat = process_line(line, tconf['columns'][0])
+        if not dat:
+            return
+        exec('modelq=Q(%s="%s")'%(tconf['updatematch'], dat))
         try:
-             data=model.objects.get(modelq)
-        except: data=None
+             data = model.objects.get(modelq)
+        except:
+            data = None
     else: 
         data=model()
     return data
                     
-def findMatchAndUpdate(tconf,data,line):
-    model=tconf['model']
-    colconf=tconf['columns'][0]
-    fu=colconf['cbyte'][0]
-    args=colconf['cbyte'][1]
-    d=fu(line,*args)
-    exec('modelq=Q(%s="%s")'%(tconf['updatematch'],d))
+def findMatchAndUpdate(tconf, data, line):
+    """
+    Don't create a new object, instead find and update
+    and old one.
+    """
+    model = tconf['model']
+    dat = process_line(line, tconf['columns'][0])
+    if not dat:
+        return 
+    # this defines modelq as a quertyset 
+    exec('modelq=Q(%s="%s")' % (tconf['updatematch'], dat))
     try:
         match=model.objects.get(modelq)
     except Exception, e: 
@@ -68,51 +84,67 @@ def findMatchAndUpdate(tconf,data,line):
         sys.stderr.write(str(e)+'\n')
 
 
-def saveAsNew(model,data):
+def saveAsNew(model, data):
+    """
+    Create a new object of type model and store
+    it in the database. 
+    """
     try:
         model.objects.create(**data)
     except Exception, e:
         sys.stderr.write(str(e)+'\n')
 
 def do_file(tconf):
-    print 'working on %s ...'%tconf['fname'],
-    model=tconf['model']
-    f=open(tconf['fname'])
-    for i in range(tconf['headlines']): f.readline()
+    """
+    Process one file definition from a config dictionary 
+
+    tconf - config dictionary (see e.g. config.py)
+    """
+    
+    print 'working on %s ...' % tconf['fname'],
+    model = tconf['model']
+    f = open(tconf['fname'])
+    for i in range(tconf['headlines']):
+        # skip header lines 
+        f.readline()
 
     data={'pk':None}
+
     for line in f:
-        if line.startswith(tconf['commentchar']): continue
+        if line.startswith(tconf['commentchar']):
+            # wean out comments 
+            continue
         for colconf in tconf['columns']:
-            fu=colconf['cbyte'][0]
-            args=colconf['cbyte'][1]
-            d=fu(line,*args)
-            if not d: continue
-            if colconf.has_key('cnull'):
-                if d==colconf['cnull']: continue
+            # process all columns, converting to fields
+
+
+            if not dat:
+                # not a valid line for whatever reason 
+                continue
+
             #if hasattr(model,colconf['cname']): ## THIS IS A FOREIGN KEY
             if colconf.has_key('references'):
-                refmodel=colconf['references'][0]
-                refcol=colconf['references'][1]
-                #ref=getattr(model,colconf['cname'])
-                
-                qexec='q=Q(%s="%s")'%(refcol,d)
-                exec(qexec)
-                try: d=refmodel.objects.get(q)
+                # this collumn references another field
+                # (i.e. a foreign key)
+                refmodel = colconf['references'][0]
+                refcol = colconf['references'][1]            
+                # create a query object q for locating
+                # the referenced model and field
+                exec('q=Q(%s="%s")' % (refcol, dat))
+                try:
+                    dat = refmodel.objects.get(q)
                 except: 
-                    d=None
-            
-            data[colconf['cname']]=d
+                    dat = None            
+            data[colconf['cname']] = dat
                 
-
         if tconf.has_key('updatematch'):
-            findMatchAndUpdate(tconf,data,line)
+            findMatchAndUpdate(tconf, data, line)
         else:
-            saveAsNew(model,data)
+            saveAsNew(model, data)
         
     print 'done.'
            
 
 def do_all(conf):
-    for fconf in conf['files']:
+    for fconf in conf:
         do_file(fconf)
