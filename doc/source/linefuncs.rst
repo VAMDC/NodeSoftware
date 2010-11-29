32ce472ad5183b307c09de6eb9c3f5de9f1096e8
Griatch <samreg@astro.uu.se>
2010-11-29 21:42:13 +0100

    Wrote documentation for the import tools. Also cleaned up some docs of the import modules in the process to make it match the docs.

---
+++ doc/source/linefuncs.rst
@@ -0,0 +1,14 @@
+.. _linefuncs:
+
+Linefuncs Documentation
+========================
+
+This page contains the Linefuncs Module documentation. See also :ref:`importing`.
+
+The :mod:`linefuncs` Module
+----------------------------
+
+.. automodule:: imptools.linefuncs
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- imptools/linefuncs.py
+++ imptools/linefuncs.py
@@ -1,22 +1,27 @@
 """
 Line functions are helper functions available to use in the mapping file.
 
-All line functions take 'linedata' as a first argument - this is either . This never needs to
-be given specifically in the mapping; the import system handles that.
+All importable line functions take 'linedata' as a first argument.
+This can be either a string (the current active line), or a list of strings in the case of lines from many files being read simultaneously. In the latter case, the argument 'filenum' is used to select the correct line. For linefuncs accepting a single line, this selection must be made in the mapping dictionary.
 
+Example of call from mapping dictionary:
 
-This can be either
-a string (the current active line), or a list of strings in the case of
-lines from many files being read simultaneously. In the latter case, the argument
-'filenum' is used to select the correct line. For linefuncs accepting a single line,
-this selection must be made in the calling wrapper function.
-
+ {'cname' : 'whatever_field_name', 
+  'cbyte' : (charrange, 56, 58)}
 
 """
 
 # is_iter checks if a variable is iterable (a list, tuple etc) or not.
 # Returns True/False.
-is_iter = lambda iterable: hasattr(iterable, '__iter__')
+def is_iter(iterable):
+    """
+    Helper function
+    
+    Checks if the given argument is iterable or not, i.e. if it
+    is a list or tuple. Strings are not considered iterable by
+    this function.
+    """    
+    return hasattr(iterable, '__iter__')
 
 # Line funcs
 
@@ -24,6 +29,7 @@ def charrange(linedata, start, end, filenum=0):
     """
     Cut out part of a line of texts based on indices.
 
+    Inputs:
      linedata (str or iterable) - current line(s) to operate on
      start, end (int) - beginning and end indices of the line
      filenum (int) - optional selection of line if linedata is an iterable
@@ -41,6 +47,7 @@ def charrange2int(linedata, start, end, filenum=0):
     """
     Cut out part of a line based on indices, return as integer
 
+    Inputs:
       linedata (str or iterable) - current line(s) to operate on
       start, end (int) - beginning and end indices of the line
       filenum (int) - optional selection of line if linedata is an iterable
@@ -54,11 +61,12 @@ def charrange2int(linedata, start, end, filenum=0):
         #print "ERROR: charrange2int: %s: %s" % (linedata, e)
         pass
         
-def bySepNr(linedata, number, sep=',',filenum=0):
+def bySepNr(linedata, number, sep=',', filenum=0):
     """
     Split a text line by sep argument and return
     the number:ed split section
 
+    Inputs:
       linedata (str or iterable) - current line(s) to operate on
       number (int) - nth section, separated by sep
       sep (str) - a separator to split by
@@ -78,11 +86,12 @@ def lineSplit(linedata, splitsep=',', filenum=0):
     Splits a line by splitsep, returns a list. The main use for this
     method is creating a many-to-many reference.
 
+    Inputs:
       linedata (str or iterable) - current line(s) to operate on
       splitsep (str) - string to split by
       filenum (int) - optional selection of line if linedata is an iterable   
 
-      Returns a list!      
+    Returns a list!      
     """    
 
     if is_iter(linedata):
