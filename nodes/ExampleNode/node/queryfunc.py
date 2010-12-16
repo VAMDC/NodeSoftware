141b5eb8e6d8d44913dd653d7777100c0359ce93
Thomas Marquart <thomas.marquart@fysast.uu.se>
2010-12-16 14:16:10 +0100

    improve ExampleNode/node/queryfunc.py

--- nodes/ExampleNode/node/queryfunc.py
+++ nodes/ExampleNode/node/queryfunc.py
@@ -1,46 +1,45 @@
 # -*- coding: utf-8 -*-
 
-# Get the models
-from django.conf import settings
 from django.utils.importlib import import_module
-MODELS=import_module(settings.NODEPKG+'.models')
-
-from nodelib.sqlparse import where2q
+from vamdctap.sqlparse import *
 from dictionaries import *
+from models import *
 
 # a straight forward example of getting the unique list of
 # states that correspond to a given list of transitions by
 # use of the inverse foreign key.
-def getVALDstates(transs):
-    q1,q2=Q(islowerstate_trans__in=transs),Q(islowerstate_trans__in=transs)
-    return State.objects.filter(q1|q2).distinct()
+#
+#def getStates(transs):
+#    q1,q2=Q(islowerstate_trans__in=transs),Q(islowerstate_trans__in=transs)
+#    return State.objects.filter(q1|q2).distinct()
     
-# For the Sources we use a python set to collect reference ids
+# Sometimes it is better to use a python set to collect reference ids
 # (which are not stored as foreignkeys in the model). This is faster
-# because there are so many Sources which are the same and by getting
-# rid of duplicates before asking the DB, we sve time.
-def getVALDsources(transs):
-    sids=set([])
-    for trans in transs:
-        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
-        sids=sids.union(s)
-    return Source.objects.filter(pk__in=sids)
+# eg. when  there are many referenced items which are the same. By getting
+# rid of duplicates before asking the DB, we save time.
+#
+#def getSources(transs):
+#    sids=set([])
+#    for trans in transs:
+#        s=set([trans.wave_ref,trans.loggf_ref,trans.lande_ref,trans.gammarad_ref,trans.gammastark_ref,trans.gammawaals_ref])
+#        sids=sids.union(s)
+#    return Source.objects.filter(pk__in=sids)
 
 
-def setupResults(sql):
-    q=where2q(sql.where,RESTRICTABLES)
-    try: q=eval(q)
-    except: return {}
-
-    transs = Transition.objects.filter(q)
-    
-    # use the functions from above  
-    sources = getVALDsources(transs)
-    states = getVALDstates(transs)
-
-    # return the dictionary as described above
-    return {\
-	'RadTrans':transs,
-	'Sources':sources,
-	'AtomStates':states,
-	}
+#def setupResults(sql):
+#    q = where2q(sql.where,RESTRICTABLES)
+#    try: q = eval(q)
+#    except: return {}
+#
+#    transs = Transition.objects.filter(q)
+#    
+#    # use the functions from above  
+#    sources = getVALDsources(transs)
+#    states = getVALDstates(transs)
+#
+#    # return the result dictionary 
+#    return {\
+#	'RadTrans':transs,
+#	'Sources':sources,
+#	'AtomStates':states,
+#	}
