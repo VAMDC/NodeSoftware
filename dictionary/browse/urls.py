27740747ba4e80530ce0201a47234d31ac84ee85
Thomas Marquart <thomas@marquart.se>
2011-01-20 17:52:51 +0100

    add log view for the dictionary plus bulk operations for the usage field of keywords in the admin interface

--- dictionary/browse/urls.py
+++ dictionary/browse/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 from models import KeyWord
+from django.contrib.admin.models import LogEntry
 
 urlpatterns = patterns('django.views.generic.list_detail',
     # Example:
     #(r'^browse/', include('dictionary.browse.urls')),
-    (r'^$', 'object_list', {'queryset':KeyWord.objects.all(), 'template_name':'dictionary/index.html'} )
+    (r'^$', 'object_list', {'queryset':KeyWord.objects.all(), 'template_name':'dictionary/index.html'} ),
+    (r'^log/', 'object_list', {'queryset':LogEntry.objects.all(), 'template_name':'dictionary/log.html'} ),
 )
