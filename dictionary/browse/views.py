77091b5cd6138521519ec493a3bdefceafd069d3
Thomas Marquart <thomas@marquart.se>
2011-01-24 09:18:15 +0100

    more dict validation, regexp

--- dictionary/browse/views.py
+++ dictionary/browse/views.py
@@ -5,14 +5,29 @@ from django.template import RequestContext
 from django.core.exceptions import ValidationError
 
 
+import re
+REGEX1=re.compile(r"""^(RETURNABLE|RESTRICTABLE)\s*=\s*\{(['"]\w*['"]\s*:\s*['"][a-zA-Z0-9_\.]*['"]\s*,\s*)*\s*\}\s*$""")
+
+
+def check_returnable(ret):
+    pass
+
+def check_restrictable(ret):
+    pass
+
+
 
 def validate_dict(data):
-#    try eval(data):
-    raise ValidationError('bla')
-    raise ValidationError('bla2')
+    errors=[]
+    if not REGEX1.match(data):
+        errors.append('Basic form not met.')
 
+    if errors: raise ValidationError(errors)
+    
 class CheckForm(forms.Form):
-    content=forms.CharField(label='Paste your dictionary',widget=forms.widgets.Textarea(attrs={'cols':'50','rows':'10'}),required=True,validators=[validate_dict])
+    content=forms.CharField(label='Paste your dictionary',
+	widget=forms.widgets.Textarea(attrs={'cols':'50','rows':'10'}),
+	required=True,validators=[validate_dict])
 
 def check(request):
     if request.method == 'POST':
