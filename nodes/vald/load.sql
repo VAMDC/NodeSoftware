5cb869537c7717eb8eae3c3c34192c11c6d7acea
Thomas Marquart <thomas.marquart@fysast.uu.se>
2011-02-09 15:05:54 +0100

    add file with LOAD DATA commands for new VALD database

---
+++ nodes/vald/load.sql
@@ -0,0 +1,8 @@
+set foreign_key_checks=0;
+load data infile '/vald/species.dat' into table species columns terminated by ';' optionally enclosed by '"';
+load data infile '/vald/references.dat' ignore into table refs columns terminated by ';' enclosed by '"' ;
+load data infile '/vald/linelists.dat' ignore into table linelists columns terminated by ';' optionally enclosed by '"';
+load data infile '/vald/manytomany_map.dat' ignore into table linelists_references columns terminated by ';' optionally enclosed by '"';
+load data infile '/vald/states.dat' ignore into table states columns terminated by ';' optionally enclosed by '"';
+load data infile '/vald/transitions.dat' ignore into table transitions columns terminated by ';' optionally enclosed by '"';
+
