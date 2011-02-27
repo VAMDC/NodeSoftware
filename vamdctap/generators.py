26073104dd7b5020648858f6e605c2d9e73ae991
Thomas Marquart <thomas.marquart@fysast.uu.se>
2011-02-27 11:51:12 +0100

    remove XsamsMolStates which is no longer used. also fix countReturnables

--- vamdctap/generators.py
+++ vamdctap/generators.py
@@ -25,7 +25,7 @@ def countReturnables(s):
     """
     count how often a certain (sub)string is in the keys of the returnables
     """
-    return len(filter(lambda key: 'atomstate' in key, DICTS.RETURNABLES.keys()))
+    return len(filter(lambda key: s in key, DICTS.RETURNABLES.keys()))
 
 def GetValue(name,**kwargs):
     """
@@ -274,112 +274,9 @@ def XsamsAtoms(Atoms):
 </Atom>"""
     yield '</Atoms>'
 
-
-def XsamsMolStates(Molecules, MoleStates, MoleQNs=None):
-    """
-    This function creates the molecular states part.
-    In its current form MoleStates contains all information
-    about the state including the species part. It does
-    not contain informations on the quantum numbers. These
-    are provided in the MoleQNs - List. Both are linked via
-    the StateId. In the first loop, the MoleQN-List copied into
-    a new list of lists using the StateID as keyword.
-    Maybe this part could be moved to the view.py in each node.
-    In this approach the molecular species information is part
-    of the MoleStates, which can be discussed, but probably 
-    this approach is faster in terms of performance and more
-    appropriate for the VO-Table output, because it reduces the
-    number of tables. Here, the MoleStates have to be sorted 
-    by Species. If we keep this approach the condition prooves
-    the identity of species should use the dictionary which
-    is currently under development
-    """
-
-    # nothing to see here if the data has no molecules
-    if not Molecules: return
-
-    # if MoleQNs was passed as None or not passed at all,
-    # it is effectively an empty list:
-    if MoleQNs is None:
-        MoleQNs = []
-
-    # rearrange QN-Lists into dictionary with stateID as key
-    QNList={}
-    for MolQN in MoleQNs:
-       G=lambda name: GetValue(name,MolQN=MolQN)
-       #print '+++G("MolQnStateID") =',G("MolQnStateID")
-       if QNList.has_key(G("MolQnStateID")):
-          QNList[G("MolQnStateID")].append(MolQN)
-          #print G("MolQnStateID")
-       else:
-          QNList[G("MolQnStateID")]=[MolQN]
-    #print 'MoleQNs is %d items long' % len(MoleQNs)
-    #print 'QNList is %d items long' % len(QNList)
-    #print QNList.keys()
-
-    yield '<Molecules>'
-    for Molecule in Molecules:
-        G=lambda name: GetValue(name, Molecule=Molecule)
-        yield '<Molecule>\n'
-        yield '<MolecularChemicalSpecies>\n'
-        yield '<OrdinaryStructuralFormula>%s</OrdinaryStructuralFormula>\n' \
-            % G("MolecularSpeciesOrdinaryStructuralFormula")
-        yield '<StoichiometricFormula>%s</StoichiometricFormula>\n' \
-            % G("MolecularSpeciesStoichiometricFormula")
-        yield '<ChemicalName>%s</ChemicalName>\n' \
-            % G("MolecularSpeciesChemicalName")
-        yield '</MolecularChemicalSpecies>\n'
-#        thisInchiKey = G("InchiKey")
-        speciesid = G("MolecularSpeciesID")
-
-        for MolState in MoleStates:
-            G = lambda name: GetValue(name, MolState=MolState)
-#            if G("InchiKey") != thisInchiKey:
-
-            if G("MolecularStateMolecularSpeciesID") != speciesid:
-                continue
-
-            yield """<MolecularState stateID="S%s">
-<Description>%s</Description>
-<MolecularStateCharacterisation>
-<StateEnergy energyOrigin="%s">
-<Value units="%s">%s</Value>
-</StateEnergy>
-<TotalStatisticalWeight>%s</TotalStatisticalWeight>
-</MolecularStateCharacterisation>
-""" %           (
-                G("MolecularStateStateID"),
-                G("MolecularStateDescription"),
-                G("MolecularStateEnergyOrigin"),
-                G("MolecularStateEnergyUnit"),
-                G("MolecularStateEnergyValue"),
-                G("MolecularStateCharacTotalStatisticalWeight")
-                )
-            if QNList.has_key(G("MolecularStateStateID")):
-#              G=lambda name: GetValue(name, MolQN=MolQN)
-
-              case=""
-              for MolQN in QNList[G("MolecularStateStateID")]:
-                G=lambda name: GetValue(name, MolQN=MolQN)
-                if G("MolQnCase")!=case :
-                   if case:
-                       yield '</%s:QNs>' % case
-                   yield '<%s:QNs> \n' % G("MolQnCase")
-                   case=G("MolQnCase")
-
-                yield """
-    <%s:%s """ % (G("MolQnCase"), G("MolQnLabel"))
-                if G("MolQnSpinRef"):
-                    yield """nuclearSpinRef="%s" """ % (G("MolQnSpinRef"))
-                if G("MolQnAttribute"):
-                    yield G("MolQnAttribute")
-                yield """>%s</%s:%s>""" \
-                    % (G("MolQnValue"),G("MolQnCase"),G("MolQnLabel") )
-
-              yield '</%s:QNs>' % case
-            yield '</MolecularState>'
-        yield '</Molecule>'
-    yield '</Molecules>'
+# ATOMS END
+# 
+# MOLECULES START
 
 def XsamsMCSBuild(Molecule):
     """
@@ -438,7 +335,7 @@ def XsamsMolecules(Molecules):
         if Molecule.States:
             for MolecularState in Molecule.States:
                 for MS in XsamsMSBuild(MolecularState):
-                    yield MS            
+                    yield MS
         yield '</Molecule>\n'
     yield '</Molecules>\n'
 
