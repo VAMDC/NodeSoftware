26674750282444c2c456075ecb12a43702e780ca
vamdc <vamdc@vamdc.(none)>
2011-03-11 12:11:35 +0100

    Added server proxy configs to git.

--- nodes/lund/load.sql
+++ nodes/lund/load.sql
@@ -1,10 +1,10 @@
 #replace with correct paths
 
 # load 
-LOAD DATA local INFILE '<abspath>/species.out' IGNORE INTO TABLE species COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA local INFILE '<abspath>/references.out' IGNORE INTO TABLE refs COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA local INFILE '<abspath>/states.out' IGNORE INTO TABLE states COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA local INFILE '<abspath>/transitions.out' IGNORE INTO TABLE transitions COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA local INFILE '/home/vamdc/NodeSoftware/nodes/lund/species.dat' IGNORE INTO TABLE species COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA local INFILE '/home/vamdc/NodeSoftware/nodes/lund/references.dat' IGNORE INTO TABLE refs COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA local INFILE '/home/vamdc/NodeSoftware/nodes/lund/states.dat' IGNORE INTO TABLE states COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA local INFILE '/home/vamdc/NodeSoftware/nodes/lund/transitions.dat' IGNORE INTO TABLE transitions COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
 
-# single state mapping
-#LOAD DATA local INFILE '<abspath>/singlestates.out' IGNORE INTO TABLE states COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
+# single state mapping - uncomment when single state file is available
+#LOAD DATA local INFILE '/home/vamdc/NodeSoftware/nodes/lund/singlestates.dat' IGNORE INTO TABLE states COLUMNS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '"';
