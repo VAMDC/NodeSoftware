6d9e8025f573feb2f3aa80f2ee36b867eb4081f5
Griatch <samreg@astro.uu.se>
2011-05-02 21:23:29 +0800

    Some fixes and adjustments to the test system.

--- nodes/vald/node/tests.py
+++ nodes/vald/node/tests.py
@@ -5,7 +5,19 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+import sys
+try:
+    from django.utils.unittest import TestCase
+except ImportError:
+    from django.test import TestCase
+try:    
+    from django.utils import unittest
+except ImportError:
+    import unittest
+from vamdctap import tests as vamdctests
+
+
+# add database-specific tests here. See example below. 
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,10 +26,16 @@ class SimpleTest(TestCase):
         """
         self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+# Don't edit the following. It allows this module and the vamdc
+# software tests to be run through the Django test runner.
 
+def suite():
+    """
+    This function is called automatically by the django test runner. 
+    This also runs the command tests defined in src/commands/default/tests.py.
+    """
+    tsuite = unittest.TestSuite()
+    tsuite.addTest(unittest.defaultTestLoader.loadTestsFromModule(sys.modules[__name__]))
+    tsuite.addTest(unittest.defaultTestLoader.loadTestsFromModule(vamdctests))
+    return tsuite
--- vamdctap/tests.py
+++ vamdctap/tests.py
@@ -1,11 +1,17 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+This is the VAMDC node software test suite. It is normally run together with
+a node's specific tests using manage.py test, but can also be run by executing 
+this file manually. 
 
-Replace these with more appropriate tests for your application.
 """
-
-from django.test import TestCase
+try:
+    from django.utils.unittest import TestCase
+except ImportError:
+    from django.test import TestCase
+try:    
+    from django.utils import unittest
+except ImportError:
+    import unittest
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,10 +20,27 @@ class SimpleTest(TestCase):
         """
         self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
 
+
+
+
+
+
+
+
+#
+# Bibtex parsing test.
+#
+
+class TestBibTex(TestCase):
+    def test_callBibTex2XML(self):
+        import bibtextools
+        tbibtex = """@article{BDMQ,\n        Author = {{Bi{\\'e}mont}, E. and {Dutrieux}, {J.-F.} and {Martin}, I. and {Quinet}, P.},\n        Date-Modified = {2010-09-27 17:27:07 +0200},\n        Doi = {10.1088/0953-4075/31/15/006},\n        Journal = {Journal of Physics B Atomic Molecular Physics},\n        Month = aug,\n        Note = {(BDMQ)},\n        Pages = {3321-3333},\n        Title = {{Lifetime calculations in Yb II}},\n        Volume = 31,\n        Year = 1998,\n        Bdsk-Url-1 = {http://dx.doi.org/10.1088/0953-4075/31/15/006}}\n"""
+        txml = u"""<Source sourceID="BExampleNode-BDMQ">\n<Authors>\n<Author><Name>E. Bi{\\\'e}mont</Name></Author><Author><Name>J.-F. Dutrieux</Name></Author><Author><Name>I. Martin</Name></Author><Author><Name>P. Quinet</Name></Author>\n</Authors><Title>Lifetime calculations in Yb II</Title>\n<Category>journal</Category>\n<Year>1998</Year>\n<SourceName>Journal of Physics B Atomic Molecular Physics</SourceName>\n<Volume>31</Volume>\n<PageBegin>3321</PageBegin>\n<PageEnd>3333</PageEnd>\n<UniformResourceIdentifier>http://dx.doi.org/10.1088/0953-4075/31/15/006</UniformResourceIdentifier>\n<DigitalObjectIdentifier>10.1088/0953-4075/31/15/006</DigitalObjectIdentifier>\n<BibTeX>@article{BDMQ,&#10;        Author = {{Bi{\\\'e}mont}, E. and {Dutrieux}, {J.-F.} and {Martin}, I. and {Quinet}, P.},&#10;        Date-Modified = {2010-09-27 17:27:07 +0200},&#10;        Doi = {10.1088/0953-4075/31/15/006},&#10;        Journal = {Journal of Physics B Atomic Molecular Physics},&#10;        Month = aug,&#10;        Note = {(BDMQ)},&#10;        Pages = {3321-3333},&#10;        Title = {{Lifetime calculations in Yb II}},&#10;        Volume = 31,&#10;        Year = 1998,&#10;        Bdsk-Url-1 = {http://dx.doi.org/10.1088/0953-4075/31/15/006}}&#10;</BibTeX></Source>"""
+        self.maxDiff = None
+        self.assertEquals(txml, bibtextools.BibTeX2XML(tbibtex))
+
+
+if __name__ == "__main__":    
+    print "You must run this module from a node with 'manage.py test'."
