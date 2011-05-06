21456bdba3eeccd653e0cf717497d2055bbce52b
Griatch <samreg@astro.uu.se>
2011-05-06 09:11:43 +0200

    A default tests class for generator testing.

--- vamdctap/tests.py
+++ vamdctap/tests.py
@@ -4,6 +4,8 @@ a node's specific tests using manage.py test, but can also be run by executing
 this file manually. 
 
 """
+
+# safely import test framework (django-version non-specific)
 try:
     from django.utils.unittest import TestCase
 except ImportError:
@@ -12,25 +14,53 @@ try:
     from django.utils import unittest
 except ImportError:
     import unittest
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-
-
-
-
-
-
-
+from django.test.client import Client
 
 
 #
-# Bibtex parsing test.
+# Setting up the testing client
+#
+TCLIENT = Client()
+
+class TapSyncTest(TestCase):
+    """
+    This class tests the return data from a VAMDC /tap/sync/ request using
+    the test client. It makes sure to weed out all the superfluos
+    data to make it easy to define and determine mismatches.
+    """
+    def setUp(self):
+        "Creates the base config for the test class."
+        self.url_prefix = "/tap/sync/"
+    
+    
+    def call(self, requeststring, tag=None, desired=""):
+        """
+        Attempts a fake "call" from the test client to the handler. If
+        gigen, the result is checked against desired,
+        otherwise it just checked so that the return is not empty.
+
+        requeststring - a string coming in through the GET statement. The full URL
+                        should not be given, only the active part after /tap/sync/.
+        """        
+        requeststring = self.url_prefix + requeststring.strip()        
+        request = TCLIENT.get(requeststring)        
+        if desired:
+            result = request.content
+            if tag:
+                # obtain only the wanted tag to check
+                pass            
+            self.assertEqual(result, desired)        
+        
+#
+# TAPservice test suite 
+#
+
+class EmptyTest(TapSyncTest):
+    def test_call(self):
+        self.call("") # test an empty call. This should fail gracefully.
+
+#
+# Bibtex perser test suite
 #
 
 class TestBibTex(TestCase):
@@ -40,6 +70,9 @@ class TestBibTex(TestCase):
         txml = u"""<Source sourceID="BExampleNode-BDMQ">\n<Authors>\n<Author><Name>E. Bi{\\\'e}mont</Name></Author><Author><Name>J.-F. Dutrieux</Name></Author><Author><Name>I. Martin</Name></Author><Author><Name>P. Quinet</Name></Author>\n</Authors><Title>Lifetime calculations in Yb II</Title>\n<Category>journal</Category>\n<Year>1998</Year>\n<SourceName>Journal of Physics B Atomic Molecular Physics</SourceName>\n<Volume>31</Volume>\n<PageBegin>3321</PageBegin>\n<PageEnd>3333</PageEnd>\n<UniformResourceIdentifier>http://dx.doi.org/10.1088/0953-4075/31/15/006</UniformResourceIdentifier>\n<DigitalObjectIdentifier>10.1088/0953-4075/31/15/006</DigitalObjectIdentifier>\n<BibTeX>@article{BDMQ,&#10;        Author = {{Bi{\\\'e}mont}, E. and {Dutrieux}, {J.-F.} and {Martin}, I. and {Quinet}, P.},&#10;        Date-Modified = {2010-09-27 17:27:07 +0200},&#10;        Doi = {10.1088/0953-4075/31/15/006},&#10;        Journal = {Journal of Physics B Atomic Molecular Physics},&#10;        Month = aug,&#10;        Note = {(BDMQ)},&#10;        Pages = {3321-3333},&#10;        Title = {{Lifetime calculations in Yb II}},&#10;        Volume = 31,&#10;        Year = 1998,&#10;        Bdsk-Url-1 = {http://dx.doi.org/10.1088/0953-4075/31/15/006}}&#10;</BibTeX></Source>"""
         self.maxDiff = None
         self.assertEquals(txml, bibtextools.BibTeX2XML(tbibtex))
+
+
+
 
 
 if __name__ == "__main__":    
