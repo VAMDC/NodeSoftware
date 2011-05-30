f73b4dc3ee2ee683a7a4cd9c7c749f15d2cc6761
Ken Smith <k.w.smith@qub.ac.uk>
2011-05-30 19:30:24 +0800

    model modifications

--- nodes/umist/node/dictionaries.py
+++ nodes/umist/node/dictionaries.py
@@ -16,7 +16,7 @@ ExampleNode dictionary definitions.
 # look at queryfuncs.py, you'll see 'Sources' being assigned)
 
 RETURNABLES = {\
-'NodeID':'ExampleNode',
+'NodeID':'umist',
 
 'SourceID':'Source.id',
 'SourceAuthorName':'Source.author',
@@ -32,58 +32,6 @@ RETURNABLES = {\
 'MethodID':'Method.id',
 'MethodCategory':'Method.category',
                            
-'AtomStateID':'AtomState.id',
-'AtomSymbol':'Atom.name',
-'AtomSpeciesID':'Atom.id',
-'AtomNuclearCharge':'Atom.atomic',
-'AtomIonCharge':'Atom.ion',
-'AtomMassNumber':'Atom.massno',
-'AtomStateLandeFactor':'AtomState.lande',
-'AtomStateLandeFactorUnit':'unitless',
-'AtomStateLandeFactorRef':'AtomState.lande_ref_id',
-'AtomStateEnergy':'AtomState.energy',
-'AtomStateEnergyRef':'AtomState.energy_ref_id',
-'AtomStateEnergyUnit':'1/cm',
-'AtomStateDescription':'',
-'AtomStateLifeTime': 'AtomState.get_best_tau()',
-'AtomStateLifeTimeMethod': 'AtomState.get_tau_ref()',
-                                                                         
-'RadTransSpeciesRef':'RadTran.species_id',
-'RadTransComments':'Wavelength is for vacuum.',
-'RadTransWavelength':'RadTran.vacwave',
-'RadTransWavelengthUnit':u'A',
-'RadTransWavelengthRef':'RadTran.wave_ref_id',
-'RadTransWavelengthMethodCategory':'experiment',
-'RadTransFinalStateRef':'RadTran.lostate.id',
-'RadTransInitialStateRef':'RadTran.upstate.id',
-'RadTransProbabilityLog10WeightedOscillatorStrength':'RadTran.loggf',
-'RadTransProbabilityLog10WeightedOscillatorStrengthUnit':'unitless',
-'RadTransProbabilityLog10WeightedOscillatorStrengthRef':'RadTran.loggf_ref_id',
-'RadTransProbabilityLog10WeightedOscillatorStrengthMethod':'RadTran.loggf_method',
-'RadTransProbabilityLog10WeightedOscillatorStrengthAccuracy':'RadTran.loggf_accur',
-
-'RadTransBroadeningNaturalLineshapeParameter':'RadTran.gammarad',
-'RadTransBroadeningNaturalLineshapeParameterName':'log(gamma)',
-'RadTransBroadeningNaturalLineshapeParameterUnit':'cm3/s',
-'RadTransBroadeningNaturalRef':'RadTran.gammarad_ref_id',
-
-'RadTransBroadeningStarkLineshapeParameter':'RadTran.gammastark',
-'RadTransBroadeningStarkLineshapeName':'lorentzian',
-'RadTransBroadeningStarkLineshapeParameterName':'log(gamma)',
-'RadTransBroadeningStarkLineshapeParameterUnit':'cm3/s',
-'RadTransBroadeningStarkRef':'RadTran.gammastark_ref_id',
-
-'RadTransBroadeningVanDerWaalsLineshapeParameter':'RadTran.gammawaals',
-'RadTransBroadeningVanDerWaalsLineshapeParameterUnit':'["cm3/s","unitless"]',
-
-'RadTransBroadeningVanDerWaalsLineshapeName':'lorentzian',
-'RadTransBroadeningVanDerWaalsLineshapeParameterName':'["log(gamma)","alpha"]',
-'RadTransBroadeningVanDerWaalsRef':'RadTran.waals_ref_id',
-
-'RadTransEffectiveLandeFactor':'RadTran.landeff',
-'RadTransEffectiveLandeFactorUnit':'unitless',
-'RadTransEffectiveLandeFactorRef':'RadTran.lande_ref_id',
-
 'FunctionID':'Function.id',
 'FunctionName':'Function.name',
 'FunctionSourceRef': "",
@@ -114,7 +62,8 @@ RETURNABLES = {\
 # for the Species.atomic field  would be written as species__atomic.
 
 RESTRICTABLES = {\
-'AtomSymbol':'species__name',
+'MoleculeInchiKey':'species__vamdc_inchikey',
+'MoleculeInchi':'species__vamdc_inchi',
 'AtomNuclearCharge':'species__atomic',
 'AtomStateEnergy':'upstate__energy',
 'RadTransWavelength':'vacwave',
