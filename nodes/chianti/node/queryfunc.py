64c86ce2e117fe5813e9cf397f0cdb44239c4249
Guy Rixon <guyrixon@gmail.com>
2011-06-09 21:27:40 +0800

    Fixed logging

--- nodes/chianti/node/queryfunc.py
+++ nodes/chianti/node/queryfunc.py
@@ -24,7 +24,7 @@ def setupResults(sql, LIMIT=1000):
     # Find transitions satisfying the criteria.
     transitions = Transitions.objects.filter(q)
     nTransitions = transitions.count()
-    LOG("Number of transitions: ", nTransitions)
+    LOG("Number of transitions: %d"%nTransitions)
 
     # Truncate the list of matching transitions.
     # Record the degree of truncation s.t. it can be reported in the output.
