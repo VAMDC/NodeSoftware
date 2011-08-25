2630b9f4eac45f0df28e949cf349e085476ab93f
Griatch <samreg@astro.uu.se>
2011-08-25 18:15:08 +0800

    Fixed wrong placement of two tags in the CrossSection generator.

--- vamdctap/generators.py
+++ vamdctap/generators.py
@@ -1164,7 +1164,6 @@ def XsamsRadCross(RadCross):
     if not isiterable(RadCross):
         return
 
-    yield "<CrossSection>"
     for RadCros in RadCross:
         cont, ret = checkXML(RadCros)
         if cont:
@@ -1222,7 +1221,7 @@ def XsamsRadCross(RadCross):
                     else:
                         yield "<DeltaV>%s</DeltaV>" % deltav                    
                 yield "</Modes>"
-            yield "</BandAssignment>"
+        yield "</BandAssignment>"
         yield "</CrossSection>"
 
 
@@ -1254,7 +1253,7 @@ def XsamsCollTrans(CollTrans):
 
     if not isiterable(CollTrans):
         return
-    yield "<Collisions>"
+    #yield "<Collisions>"
     for CollTran in CollTrans:
 
         cont, ret = checkXML(CollTran)
