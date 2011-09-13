85ba85697c17ed06db65517d6a8c153e0db9734c
Thomas Marquart <thomas@marquart.se>
2011-09-13 11:40:46 +0200

    get query_preview.py working to print headers from all nodes

--- other/query_preview.py
+++ other/query_preview.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
 import sys
 import requests
-from multiprocessing import Pool
 from vamdc.other.registry import getNodeList
 
 try:
     QUERY = sys.argv[1]
 except:
-    print "Please give the complete query string as an argument, in double quotes."
+    print "Please give a query string as an argument, in double quotes."
     sys.exit(1)
 
 PARAMS = {\
@@ -16,22 +19,22 @@ PARAMS = {\
     'FORMAT':'XSAMS',
     'LANG':'VSS2'
     }
+TIMEOUT = 5
 
-def headreq(nodes):
-    return requests.head(nodes['url']+'sync',params=PARAMS,timeout=5)
+def headreq(node):
+    return requests.head(node['url']+'sync',params=PARAMS,timeout=TIMEOUT)
 
 def run():
     nodes = getNodeList()
-    n = len(nodes)
-    pool = Pool(processes=n)
-
-    pardict = {'url':[node['url']+'sync' for node in nodes],
-               'params':(PARAMS,)*n,
-               'timeout':(5,)*n
-            }
-
-    res = pool.map(headreq,nodes)
-    print res
+    jobs = [gevent.spawn(headreq,node) for node in nodes]
+    gevent.joinall(jobs, timeout=TIMEOUT+1)
+    results = [job.value for job in jobs if job.value]
+    for result in results:
+        if result.status_code != 200: continue
+        print '\n',result.url
+        for head in result.headers:
+            if head.lower().startswith('vamdc'):
+                print head, result.headers[head]
 
 if __name__ == '__main__':
     run()
