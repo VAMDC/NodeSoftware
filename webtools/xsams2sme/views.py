bd84f7e3edd529fa24ed66e85d3d90377be62395
Thomas Marquart <thomas.marquart@fysast.uu.se>
2011-09-14 23:39:53 +0200

    better eoor handling and layout for xsams2sme

--- webtools/xsams2sme/views.py
+++ webtools/xsams2sme/views.py
@@ -13,22 +13,23 @@ from urllib2 import urlopen
 
 class ConversionForm(Form):
     infile = FileField(label='Input file',required=False)
-    inurl = URLField(label='Input URL',required=False,widget=TextInput(attrs={'size': 100, 'title': 'Paste here a URL that delivers an XSAMS document.',}))
+    inurl = URLField(label='Input URL',required=False,widget=TextInput(attrs={'size': 50, 'title': 'Paste here a URL that delivers an XSAMS document.',}))
 
     def clean(self):
-        cleaned_data = self.cleaned_data
-        if cleaned_data.get('infile') and cleaned_data.get('inurl')\
-            or (not (cleaned_data.get('infile') or cleaned_data.get('inurl'))):
+        infile = self.cleaned_data.get('infile')
+        inurl = self.cleaned_data.get('inurl')
+        if (infile and inurl) or (not (infile or inurl)):
             raise ValidationError('Give either input file or URL!')
 
-        return cleaned_data
+        return self.cleaned_data
 
-def handle_file(data):
-    return xsl(e.parse(data))
-
-def handle_url(url):
-    data = urlopen(url)
-    return xsl(e.parse(data))
+def transform(data):
+    try: xml=e.parse(data)
+    except Exception,err:
+        raise ValidationError('Could not parse XML file: %s'%err)
+    try: return xsl(xml)
+    except Exception,err:
+        raise ValidationError('Could not transform XML file: %s'%err)
 
 def xsams2sme(request):
     if request.method != 'POST':
@@ -39,9 +40,12 @@ def xsams2sme(request):
             infile=ConvForm.cleaned_data.get('infile')
             inurl=ConvForm.cleaned_data.get('inurl')
             if infile:
-                data = handle_file(infile)
+                data = transform(infile)
             elif inurl:
-                data = handle_url(inurl)
+                try: data = urlopen(inurl)
+                except Exception,err:
+                    raise ValidationError('Could not open given URL: %s'%err)
+                data = transform(data)
             response=HttpResponse(data,mimetype='text/csv')
             response['Content-Disposition'] = \
                 'attachment; filename=%s.sme'% (getattr(infile, 'name', None) or 'output')
