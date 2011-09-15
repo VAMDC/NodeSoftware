9efca010c9607b800f90f37d170451ffb26023b5
Thomas Marquart <thomas.marquart@fysast.uu.se>
2011-09-15 16:05:25 +0200

    remove custom scripts from fullimport.sh since they are no longer needed.

--- nodes/vald/fullimport.sh
+++ nodes/vald/fullimport.sh
@@ -15,13 +15,6 @@ pypy run_rewrite.py ../nodes/vald/mapping_vald3.py
 cd ../nodes/vald/
 echo "done."
 
-echo -n "Running custom scripts... "
-pypy linelists_references.py /vald/vamdc/raw_vald_data/VALD3.cfg /vald/vamdc/raw_vald_data/VALD3linelists.txt /vald/vamdc/raw_vald_data/VALD3_ref.bib
-mv linelists_references.dat /vald/vamdc/db_input_files/
-#pypy species_components.py /vald/vamdc/raw_vald_data/VALD_list_of_species
-#mv species_components.dat /vald/vamdc/db_input_files/
-echo "done."
-
 echo "Dropping and re-creating the database... "
 echo "DROP DATABASE $DB;" | mysql -u "$usr" -p"$PWD"
 echo "CREATE DATABASE $DB;" | mysql -u "$usr" -p"$PWD"
