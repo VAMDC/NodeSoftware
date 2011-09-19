18dcb0f68b5ebe6d1d6c3a14472bc431e8e50f74
Guy Rixon <guyrixon@gmail.com>
2011-09-19 14:49:03 +0100

    Fixed the -1 magic numbers in the oscillator strengths and state energies: replaced with nulls.

--- nodes/chianti/ingest-chianti.pl
+++ nodes/chianti/ingest-chianti.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 print "CREATE TABLE states (\n";
-print "         id INTEGER, \n";
 print "		ChiantiIonType CHAR(1), \n";
 print "         species INTEGER, \n";
 print "		AtomSymbol CHAR(8), \n";
@@ -14,41 +13,47 @@ print "		AtomStateL FLOAT, \n";
 print "		AtomStateTotalAngMom FLOAT, \n";
 print "		AtomStateEnergyExperimental DOUBLE, \n";
 print "		AtomStateEnergyTheoretical DOUBLE, \n";
+print "         id INTEGER, \n";
 print "		PRIMARY KEY (id) \n";
 print ");\n";
 
 
-open STATES, "/home/guy/chianti/raw-data/chianti_data_v6_e.txt" or die;
+open STATES, "/users/guy/desktop/chianti/chianti_data_v6_e.txt" or die;
 
 # Throw away the first line, which is a comment.
 $discard = <STATES>;
 
 $index = 0;
 while(<STATES>) {
-	$index++;
 	#print $_;
 	$line = $_;
 	chomp $line;
 	@fields = split(/\s*\|\s*/, $line);
 	#print join ":", @fields, "\n";
 	print 'INSERT INTO states VALUES(';
-	print $index, ', '; # Primary key
 	print '"', shift @fields, '", '; # ChiantiIonType
         print '0, '; # species reference - filled in later
 	print '"', firstWord(shift @fields), '", '; # AtomSymbol
-	print shift @fields, ', '; # AtomNuclearCharge
-	print shift @fields, ', '; # AtomIonCharge
-	print shift @fields, ', '; # ChiantiAtomStateIndex
+	$nuclearCharge = shift @fields; print $nuclearCharge, ', '; # AtomNuclearCharge
+	$ionCharge     = shift @fields; print $ionCharge, ', '; # AtomIonCharge
+	$stateIndex    = shift @fields; print $stateIndex, ', '; # ChiantiAtomStateIndex
 	print '"', shift @fields, '", '; # AtomStateConfigurationLabel
 	print shift @fields, ', '; # AtomStateS
 	print shift @fields, ', '; # AtomStateL
 	print shift @fields, ', '; # AtomStateTotalAngMom
 	print shift @fields, ', '; # AtomStateEnergyExperimentalValue
-	print shift @fields; # AtomStateEnergyTheoreticalValue
+	print shift @fields, ', '; # AtomStateEnergyTheoreticalValue
+	$index = (1000000 * $stateIndex) + (1000 * $ionCharge) + $nuclearCharge;
+	print $index; # id - primary key
         print ");\n";
 }
 
 close STATES;
+
+# Unknown energies are represented in the input by negative values; convert them to nulls.
+print "UPDATE states SET AtomStateEnergyExperimental = NULL WHERE AtomStateEnergyExperimental < 0.0;\n";
+print "UPDATE states SET AtomStateEnergyTheoretical = NULL WHERE AtomStateEnergyTheoretical < 0.0;\n";
+
 
 
 print "CREATE TABLE transitions (\n";
@@ -64,7 +69,7 @@ print "		RadTransProbabilityTransitionProbabilityA DOUBLE, \n";
 print "         PRIMARY KEY (id) \n";
 print ");\n";
 
-open TRANSITIONS, "/home/guy/chianti/raw-data/chianti_data_v6_l-1.txt" or die;
+open TRANSITIONS, "/users/guy/desktop/chianti/chianti_data_v6_l-1.txt" or die;
 
 # Throw away the first line, which is a comment.
 $discard = <TRANSITIONS>;
@@ -77,10 +82,10 @@ while(<TRANSITIONS>) {
 	my @fields = split(/\s*\|\s*/, $line);
         my $transTypeCode = shift @fields;
         my $atomSymbol = firstWord(shift @fields);
-        shift @fields; # AtomNuclearCharge - unwanted
-        shift @fields; # AtomIonCharge - unwanted
-        my $finalStateIndex = shift @fields;
-        my $initialStateIndex = shift @fields;
+        $nuclearCharge = shift @fields; # AtomNuclearCharge - unwanted
+        $ionCharge     = shift @fields; # AtomIonCharge - unwanted
+        my $finalStateIndex   = (1000000 * (shift @fields)) + (1000 * $ionCharge) + $nuclearCharge;
+        my $initialStateIndex = (1000000 * (shift @fields)) + (1000 * $ionCharge) + $nuclearCharge;;
         my $experimentalWavelength = shift @fields;
         my $theoreticalWavelength = shift @fields;
         my $weightedOscilatorStrength = shift @fields;
@@ -117,6 +122,10 @@ while(<TRANSITIONS>) {
 	}
 }
 
+# In the input, unknown oscillator strengths are denoted by -1. Change these to proper nulls.
+print "UPDATE transitions SET RadTransProbabilityWeightedOscillatorStrength = NULL WHERE RadTransProbabilityWeightedOscillatorStrength < 0.0;\n";
+
+
 # Set the foreign key pointing from the states table into the species table.
 print "UPDATE states SET species=(1000*AtomIonCharge) + AtomNuclearCharge;\n";
 
@@ -140,5 +149,3 @@ sub firstWord {
 	my @words = split /\s+/, $sentence;
         return shift @words;
 }
-
-
