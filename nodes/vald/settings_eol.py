311e9e559f72e15486de1b4ad5d0a6b8dd23c87a
Thomas Marquart <thomas.marquart@fysast.uu.se>
2011-09-20 17:00:35 +0200

    use DB vald instead of valdx

--- nodes/vald/settings_eol.py
+++ nodes/vald/settings_eol.py
@@ -7,33 +7,15 @@ TRANSLIM = 6000
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'valdx',
+    'NAME': 'vald',
     'USER': 'vald',
     'PASSWORD': 'V@ld',
-#    'OPTIONS': {
-#           "init_command": "SET storage_engine=INNODB",
-#    }   
   },
   'valdx': {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'valdx',
     'USER': 'vald',
     'PASSWORD': 'V@ld',
-  },
-  'test': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'test',
-    'USER': 'vald',
-    'PASSWORD': 'V@ld',
-  },
-  'memory': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'vald',
-    'USER': 'vald',
-    'PASSWORD': 'V@ld',
-    'OPTIONS': {
-           "init_command": "SET storage_engine=MEMORY",
-    }
   },
   'sqlite': {
     'ENGINE': 'django.db.backends.sqlite3',
