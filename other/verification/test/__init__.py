d87b47a616b86992d12e5b77e30926d9342a35ed
Alexey Privezentsev <remake@iao.ru>
2011-10-18 17:35:19 +0800

    Add verification quantum numbers for C2H2 molecules

--- other/verification/test/__init__.py
+++ other/verification/test/__init__.py
@@ -1,2 +1,106 @@
+from xml.dom import minidom, Node
+from xml import xpath, ns
+
+
 __author__ = 'aip'
-  
+
+def makeTestFile(fileName, count = 0):
+    doc = minidom.parse(open("test/" + fileName + ".xml"))
+
+    root = doc._get_documentElement()
+    schemaLocationAttr = root.getAttributeNodeNS(ns.SCHEMA.XSI3, 'schemaLocation')
+    locations = schemaLocationAttr.value.split(' ')
+    locations[1] = '../xsd/xsams/0.2/xsams.xsd'
+    schemaLocationAttr.value = locations[0] + ' ' + locations[1]
+
+    nodes = xpath.Evaluate('//*[child::RadiativeTransition]', doc)
+    whiteListOfRefs = _getWhiteListOfRefs(nodes, ['Probability', 'Broadening', 'Shifting'], count)
+    if not whiteListOfRefs:
+        nodes = xpath.Evaluate('//*[child::NonRadiativeTransition]', doc)
+        whiteListOfRefs = _getWhiteListOfRefs(nodes, ['Probability', 'Broadening', 'Shifting'], count)
+        if not whiteListOfRefs:
+            nodes = xpath.Evaluate('//*[child::MolecularState]', doc)
+            whiteListOfRefs = _getWhiteListOfRefs(nodes, [], count)
+            if not whiteListOfRefs:
+                nodes = xpath.Evaluate('//*[child::AtomicState]', doc)
+                whiteListOfRefs = _getWhiteListOfRefs(nodes, [], count)
+
+    _removeRedundantParentNodes(nodes, doc, whiteListOfRefs)
+
+    xml = doc.toxml()
+    #print xml
+
+    out = open("test/" + fileName + ".IN.xml", 'w')
+    out.write(xml)
+    out.close()
+
+    out = open("test/" + fileName + ".OUT.MIN.xml", 'w')
+    out.write(xml)
+    out.close()
+
+    return doc
+
+def _getWhiteListOfRefs(nodes, ballastList, count = 0):
+
+    whiteListOfRefs = {}
+    if nodes:
+        for node in nodes:
+            i = 0
+            for childNode in node.childNodes[:]:
+                    if not count or i < count:
+                        if childNode.nodeType == Node.ELEMENT_NODE:
+
+                            for ballastName in ballastList:
+                                for ballastNode in childNode.getElementsByTagName(ballastName):
+                                    childNode.removeChild(ballastNode)
+
+                            for refNode in xpath.Evaluate('.//*[contains(local-name(@*), "Ref") or contains(local-name(), "Ref")]', childNode):
+                                if refNode.hasAttributes():
+                                    for attribute in refNode._attrs:
+                                        if attribute.endswith('Ref'):
+                                            whiteListOfRefs[refNode._attrs[attribute].value] = None
+                                elif refNode.nodeType == Node.ELEMENT_NODE and refNode.nodeName.endswith('Ref') and refNode.hasChildNodes():
+                                    if refNode.childNodes[0].nodeType == Node.TEXT_NODE:
+                                        whiteListOfRefs[refNode.childNodes[0].data] = None
+                            i += 1
+                    else:
+                        node.removeChild(childNode)
+
+    return whiteListOfRefs
+
+def _removeRedundantParentNodes(usefulParentNodes, stopNode, whiteListOfRefs):
+    if not usefulParentNodes or stopNode in usefulParentNodes:
+        return
+
+    parentsOfNodes = {}
+    for usefulParentNode in usefulParentNodes:
+        if usefulParentNode.parentNode and not parentsOfNodes.has_key(usefulParentNode.parentNode):
+            parentsOfNodes[usefulParentNode.parentNode] = None
+
+            childNodeNames = [childNode.nodeName for childNode in usefulParentNode.parentNode.childNodes]
+            singleUseNodeNames = [childNodeName for childNodeName in childNodeNames if childNodeNames.count(childNodeName) == 1]
+
+            flag = True
+            while flag:
+                flag = False
+                for childNode in usefulParentNode.parentNode.childNodes[:]:
+                    if not (childNode in usefulParentNodes):
+                        hasIDs = False
+                        isRedundantNode = True
+                        idNodes = {}
+                        for idNode in xpath.Evaluate('descendant-or-self::node()[contains(local-name(@*), "ID")]', childNode):
+                            if idNode.hasAttributes():
+                                for attribute in idNode._attrs:
+                                    if attribute.endswith('ID'):
+                                        hasIDs = True
+                                        if whiteListOfRefs.has_key(idNode._attrs[attribute].value):
+                                            isRedundantNode = False
+                                            idNodes[idNode] = None
+
+                        if isRedundantNode:
+                            if hasIDs or not (childNode.nodeName in singleUseNodeNames):
+                                usefulParentNode.parentNode.removeChild(childNode)
+                        else:
+                            _removeRedundantParentNodes(idNodes, childNode, whiteListOfRefs)
+
+    _removeRedundantParentNodes(parentsOfNodes, stopNode, whiteListOfRefs)
--- other/verification/test/testVerification.py
+++ other/verification/test/testVerification.py
@@ -3,13 +3,10 @@ import unittest
 
 import sys
 
-
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
 import verification
-from xml import xpath
-
 
 verification.VERIFICATION_SCHEMA_LOCATION = u"../verification.xsd"
 
@@ -20,25 +17,23 @@ class VerificationTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-
     def testRunRulesTransition1MinOK(self):
         ver = Verification(open("test/transition1OK.IN.xml"))
-        ver.onlyRules = {"RuleTA02F": {"copy": "RuleTA02", "forInChIList": "InChI=1S/Fe/q+3/"}}
+        ver.onlyRules = {"atomRuleT02F": {"copy": "atomRuleT02", "forInChIList": "InChI=1S/Fe/q+3/"}}
         doc = ver.run(False, True)
         self.assertEquals(etree.tostring(objectify.fromstring(open("test/transition1OK.OUT.MIN.xml").read())), etree.tostring(objectify.fromstring(doc.toxml())))
 
     def testGetRules(self):
         rules = VerificationParser().getRules()
         for rule in rules:
-            self.assertEquals("Rule", rule[0:4])
-            self.assertTrue(rules[rule]['math'].serialize("python").find('@InitialStateRef'))
+            self.assertNotEquals(rule.find("Rule"), -1)
+            self.assertTrue(rules[rule]['math'].serialize("python").find('#InitialStateRef'))
 
     def testRunRulesTransition3MaxOK(self):
         ver = Verification(open("test/transition3OK.IN.xml"))
-        ver.onlyRules = {"RuleTA01": {}, "RuleTA02": {}}
+        ver.onlyRules = {"atomRuleT01": {}, "atomRuleT02": {}}
         doc = ver.run(True, False)
         self.assertEquals(etree.tostring(objectify.fromstring(open("test/transition3OK.OUT.MAX.xml").read())), etree.tostring(objectify.fromstring(doc.toxml())))
-
 
     def testRunRulesTransition3MinOK(self):
         ver = Verification(open("test/transition3OK.IN.xml"))
@@ -55,7 +50,7 @@ class VerificationTestCase(unittest.TestCase):
     def testRunRulesMoleculeCO2MinOK(self):
         ver = Verification(open("test/moleculeCO2.IN.xml"))
         #ver.printRules()
-        #ver.onlyRules= {"RuleSC04":{}}
+        #ver.onlyRules= {"ltcsRuleS04":{}}
         doc = ver.run(False, True)
         self.assertEquals(etree.tostring(objectify.fromstring(open("test/moleculeCO2.OUT.MIN.xml").read())), etree.tostring(objectify.fromstring(doc.toxml())))
 
@@ -72,113 +67,19 @@ class VerificationTestCase(unittest.TestCase):
         doc = ver.run()
         self.assertEquals(etree.tostring(objectify.fromstring(open("test/moleculeCO.OUT.MIN.xml").read())), etree.tostring(objectify.fromstring(doc.toxml())))
 
+    def testRunRulesMoleculeC2H2MinOK(self):
+        ver = Verification(open("test/moleculeC2H2.IN.xml"))
+        #ver.onlyRules= {"lpcsRuleS02":{}}
+        #ver.printRules()
+        doc = ver.run()
+        self.assertEquals(etree.tostring(objectify.fromstring(open("test/moleculeC2H2.OUT.MIN.xml").read())), etree.tostring(objectify.fromstring(doc.toxml())))
+
 
     def tearDown(self):
         pass
 
 
-
-def makeTestFile(fileName, count = 0):
-    doc = minidom.parse(open("test/" + fileName + ".xml"))
-
-    root = doc._get_documentElement()
-    schemaLocationAttr = root.getAttributeNodeNS('http://www.w3.org/2001/XMLSchema-instance', 'schemaLocation')
-    locations = schemaLocationAttr.value.split(' ')
-    locations[1] = '../xsd/xsams/0.2/xsams.xsd'
-    schemaLocationAttr.value = locations[0] + ' ' + locations[1]
-
-    nodes = xpath.Evaluate('//*[child::RadiativeTransition]', doc)
-    whiteListOfRefs = _getWhiteListOfRefs(nodes, ['Probability', 'Broadening', 'Shifting'], count)
-    if not whiteListOfRefs:
-        nodes = xpath.Evaluate('//*[child::NonRadiativeTransition]', doc)
-        whiteListOfRefs = _getWhiteListOfRefs(nodes, ['Probability', 'Broadening', 'Shifting'], count)
-        if not whiteListOfRefs:
-            nodes = xpath.Evaluate('//*[child::MolecularState]', doc)
-            whiteListOfRefs = _getWhiteListOfRefs(nodes, [], count)
-            if not whiteListOfRefs:
-                nodes = xpath.Evaluate('//*[child::AtomicState]', doc)
-                whiteListOfRefs = _getWhiteListOfRefs(nodes, [], count)
-
-    _removeRedundantParentNodes(nodes, doc, whiteListOfRefs)
-
-    xml = doc.toxml()
-    #print xml
-
-    out = open("test/" + fileName + ".IN.xml", 'w')
-    out.write(xml)
-    out.close()
-
-    out = open("test/" + fileName + ".OUT.MIN.xml", 'w')
-    out.write(xml)
-    out.close()
-
-    return doc
-
-def _getWhiteListOfRefs(nodes, ballastList, count = 0):
-
-    whiteListOfRefs = {}
-    if nodes:
-        for node in nodes:
-            i = 0
-            for childNode in node.childNodes[:]:
-                    if not count or i < count:
-                        if childNode.nodeType == Node.ELEMENT_NODE:
-
-                            for ballastName in ballastList:
-                                for ballastNode in childNode.getElementsByTagName(ballastName):
-                                    childNode.removeChild(ballastNode)
-
-                            for refNode in xpath.Evaluate('.//*[contains(local-name(@*), "Ref") or contains(local-name(), "Ref")]', childNode):
-                                if refNode.hasAttributes():
-                                    for attribute in refNode._attrs:
-                                        if attribute.endswith('Ref'):
-                                            whiteListOfRefs[refNode._attrs[attribute].value] = None
-                                elif refNode.nodeType == Node.ELEMENT_NODE and refNode.nodeName.endswith('Ref') and refNode.hasChildNodes():
-                                    if refNode.childNodes[0].nodeType == Node.TEXT_NODE:
-                                        whiteListOfRefs[refNode.childNodes[0].data] = None
-                            i += 1
-                    else:
-                        node.removeChild(childNode)
-
-    return whiteListOfRefs
-
-def _removeRedundantParentNodes(usefulParentNodes, stopNode, whiteListOfRefs):
-    if not usefulParentNodes or stopNode in usefulParentNodes:
-        return
-
-    parentsOfNodes = {}
-    for usefulParentNode in usefulParentNodes:
-        if usefulParentNode.parentNode and not parentsOfNodes.has_key(usefulParentNode.parentNode):
-            parentsOfNodes[usefulParentNode.parentNode] = None
-
-            childNodeNames = [childNode.nodeName for childNode in usefulParentNode.parentNode.childNodes]
-            singleUseNodeNames = [childNodeName for childNodeName in childNodeNames if childNodeNames.count(childNodeName) == 1]
-
-            flag = True
-            while flag:
-                flag = False
-                for childNode in usefulParentNode.parentNode.childNodes[:]:
-                    if not (childNode in usefulParentNodes):
-                        hasIDs = False
-                        isRedundantNode = True
-                        idNodes = {}
-                        for idNode in xpath.Evaluate('descendant-or-self::node()[contains(local-name(@*), "ID")]', childNode):
-                            if idNode.hasAttributes():
-                                for attribute in idNode._attrs:
-                                    if attribute.endswith('ID'):
-                                        hasIDs = True
-                                        if whiteListOfRefs.has_key(idNode._attrs[attribute].value):
-                                            isRedundantNode = False
-                                            idNodes[idNode] = None
-
-                        if isRedundantNode:
-                            if hasIDs or not (childNode.nodeName in singleUseNodeNames):
-                                usefulParentNode.parentNode.removeChild(childNode)
-                        else:
-                            _removeRedundantParentNodes(idNodes, childNode, whiteListOfRefs)
-
-    _removeRedundantParentNodes(parentsOfNodes, stopNode, whiteListOfRefs)
-
+#from test import makeTestFile
 #makeTestFile('moleculeCO', 3)
 
 if __name__ == '__main__':
