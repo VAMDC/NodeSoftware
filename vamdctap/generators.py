98cbea485da82ae3f7a005f6ac4e48e9a5646e5b
nicolas <nicolas.moreau@obspm.fr>
2012-01-30 16:33:08 +0100

    modify accuracy generation in radtransshifting name is no more a mandatory parameter

--- vamdctap/generators.py
+++ vamdctap/generators.py
@@ -1138,6 +1138,8 @@ def XsamsRadTranBroadening(G):
         else:
             s.append( makeBroadeningType(G, name=broadening) )
     return ''.join(s)
+    
+
 
 
 def XsamsRadTranShifting(RadTran):
@@ -1153,8 +1155,6 @@ def XsamsRadTranShifting(RadTran):
             eref = G("RadTransShiftingEnv")
             if nam:
                 dic["name"] = nam
-            #else:
-            #    continue
             if eref:
                 dic["envRef"] = "E%s-%s"  % (NODEID, eref)
             string += makePrimaryType("Shifting", "RadTransShifting", G, extraAttr=dic)
@@ -1162,23 +1162,11 @@ def XsamsRadTranShifting(RadTran):
                 for ShiftingParam in Shifting.ShiftingParams:
                     GS = lambda name: GetValue(name, ShiftingParam=ShiftingParam)
                     string += makePrimaryType("ShiftingParameter", "RadTransShiftingParam", GS, extraAttr={"name":GS("RadTransShiftingParamName")})
-                    val = GS("RadTransShiftingParamUnits")
-
+                    val = GS("RadTransShiftingParamUnit")
                     if val:
-                        string += "<Value units='%s'>%s</Value>" % (GS("RadTransShiftingParamUnits"), GS("RadTransShiftingParam" ))
-                        string += makePrimaryType("Accuracy", "RadTransShiftingParamAcc" , GS, extraAttr={"calibration":GS("RadTransShiftingParamAccCalib" ), "quality":GS("RadTransShiftingParamAccQuality")})
-                        systerr = GS("RadTransShiftingParamAccSystematic")
-                        if systerr:
-                            string += "<Systematic confidence=%s relative=%s>%s</Systematic>" % (GS("RadTransShiftingParamAccSystematicConfidence"), GS("RadTransShiftingParamAccSystematicRelative"), systerr)
-                        staterr = GS("RadTransShiftingParamAccStatistical")
-                        if staterr:
-                            string += "<Statistical confidence=%s relative=%s>%s</Statistical>" % (GS("RadTransShiftingParamAccStatisticalConfidence"), GS("RadTransShiftingParamAccStatisticalRelative"), staterr)
-                        stathigh = GS("RadTransShiftingParamAccStatHigh")
-                        statlow = GS("RadTransShiftingParamAccStatLow")
-                        if stathigh and statlow:
-                            string += "<StatHigh confidence=%s relative=%s>%s</StatHigh>" % (GS("RadTransShiftingParamAccStatHighConfidence"), GS("RadTransShiftingParamAccStatHighRelative"), systerr)
-                            string += "<StatLow confidence=%s relative=%s>%s</StatLow>" % (GS("RadTransShiftingParamAccStatLowConfidence"), GS("RadTransShiftingParamAccStatLowRelative"), systerr)
-                        string += "</Accuracy>"
+                        string += "<Value units='%s'>%s</Value>" % (val, GS("RadTransShiftingParam" ))
+                    string += makeAccuracy('RadTransShiftingParam', GS)
+                    
 
                     if hasattr(ShiftingParam, "Fit"):
                         for Fit in makeiter(ShiftingParam.Fits):
