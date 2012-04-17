87becf8eb48487de07fd3e7077f49448c573c5fd
griatch <samreg@astro.uu.se>
2012-04-17 17:23:56 +0200

    Updated the VALD node to the latest database dump, including accuracies and transition types as well as extra wavelength info. The mapping file handles the new format of term-file and species-file. Dictionary have been updated to handle the new structure, but a few fields are not read yet, such as multiple wavelengths (not supported by generator?) and transition type.

--- nodes/vald/load.sql
+++ nodes/vald/load.sql
@@ -1,3 +1,5 @@
+alter table transitions disable keys;
+
 load data infile '/vald/vamdc/db_input_files/species.dat' into table species columns terminated by ';' optionally enclosed by '"';
 load data infile '/vald/vamdc/db_input_files/species_components.dat' into table species_components columns terminated by ';' optionally enclosed by '"';
 load data infile '/vald/vamdc/db_input_files/references.dat' ignore into table refs columns terminated by ';' enclosed by '"' ;
@@ -6,19 +8,24 @@ load data infile '/vald/vamdc/db_input_files/linelists_references.dat' ignore in
 
 alter table states ROW_FORMAT = FIXED;
 alter table states modify id varchar(64) NOT NULL;
+
 load data infile '/vald/vamdc/db_input_files/upstates.dat' ignore into table states columns terminated by ';' optionally enclosed by '"';
 load data infile '/vald/vamdc/db_input_files/lowstates.dat' ignore into table states columns terminated by ';' optionally enclosed by '"';
+
 create temporary table `sids` (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, charid VARCHAR(64) NOT NULL UNIQUE);
 insert into sids (charid) select id from states;
 update states,sids set states.id=sids.id where states.id=sids.charid;
 alter table states modify id INT NOT NULL;
-
 alter table transitions ROW_FORMAT = FIXED;
+
 alter table transitions modify upstate varchar(64);
 alter table transitions modify lostate varchar(64);
+
 load data infile '/vald/vamdc/db_input_files/transitions.dat' ignore into table transitions columns terminated by ';' optionally enclosed by '"';
+
 update transitions,sids set transitions.upstate=sids.id where transitions.upstate=sids.charid;
 update transitions,sids set transitions.lostate=sids.id where transitions.lostate=sids.charid;
+
 alter table transitions modify upstate INT;
 alter table transitions modify lostate INT;
 
@@ -33,3 +40,4 @@ update transitions t set t.method_restrict='5' where t.method_return='5';
 update transitions t, states s set t.einsteina=(0.667025*POWER(10,16) * POWER(10,t.loggf)) / ((2.0 * s.j + 1.0) * POWER(t.wave,2)) where t.upstate=s.id;
 
 create index speciesid_wave on transitions (species_id,wave);
+alter table transitions enable keys;
