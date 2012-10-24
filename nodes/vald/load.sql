55dac65985455798a5871e46dd9713d193534e01
griatch <samreg@astro.uu.se>
2012-10-24 10:53:20 +0200

    Set up things for latest VALD3 import.

--- nodes/vald/load.sql
+++ nodes/vald/load.sql
@@ -9,20 +9,19 @@ load data infile '/vald/vamdc/db_input_files/references.dat' ignore into table r
 load data infile '/vald/vamdc/db_input_files/linelists.dat' ignore into table linelists columns terminated by ';' optionally enclosed by '"';
 load data infile '/vald/vamdc/db_input_files/linelists_references.dat' ignore into table linelists_references columns terminated by ';' optionally enclosed by '"';
 
-
 load data infile '/vald/vamdc/db_input_files/upstates.dat' ignore into table states columns terminated by ';' optionally enclosed by '"';
 load data infile '/vald/vamdc/db_input_files/lowstates.dat' ignore into table states columns terminated by ';' optionally enclosed by '"';
 
 load data infile '/vald/vamdc/db_input_files/transitions.dat' ignore into table transitions columns terminated by ';' optionally enclosed by '"';
 
-update transitions t, states s set t.einsteina=(0.667025*POWER(10,16) * POWER(10,t.loggf)) / ((2.0 * s.j + 1.0) * POWER(t.wavevac,2)) where t.upstate=s.id;
+update transitions t, states s set t.einsteina=(0.667025*POWER(10,16) * POWER(10,t.loggf)) / ((2.0 * s.j + 1.0) * POWER(t.wave,2)) where t.upstate=s.id;
 
-create index speciesid_wave on transitions (species_id, wavevac);
+create index speciesid_wave on transitions (species_id, wave);
 
 -- fixing a mal-named bibtex reference (kept here for reference, remove
 -- if fixed in raw data dump next update)
-update transitions t set t.wavevac_ref_id="K11" where t.wavevac_ref_id="K11P";
-update transitions t set t.waveair_ref_id="K11" where t.waveair_ref_id="K11P";
+update transitions t set t.wave_ref_id="K11" where t.wave_ref_id="K11P";
+update transitions t set t.waveritz_ref_id="K11" where t.waveritz_ref_id="K11P";
 update transitions t set t.loggf_ref_id="K11" where t.loggf_ref_id="K11P";
 update transitions t set t.gammarad_ref_id="K11" where t.gammarad_ref_id="K11P";
 update transitions t set t.gammastark_ref_id="K11" where t.gammastark_ref_id="K11P";
