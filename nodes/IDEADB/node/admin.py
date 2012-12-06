292ff8ecb252659b274a77b0f9b34d7e10eaadb9
johannespostler <johannes.postler@uibk.ac.at>
2012-12-06 16:47:27 +0100

    Vastly improve admin interface using some simple tools provided by Django.

--- nodes/IDEADB/node/admin.py
+++ nodes/IDEADB/node/admin.py
@@ -3,12 +3,24 @@ from django.contrib import admin
 #import your models here like this:
 from IDEADB.node.models import *
 
-# add your models here to have them in the admin interface
-# for example if you have a model class called "Transition":
-admin.site.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ('chemical_formula', 'name', 'mass')
+    search_fields = ('chemical_formula', 'name', 'mass')
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'journal', 'year')
+    search_fields = ('title', 'journal', 'year')
+    filter_horizontal = ('authors',)
+
+class EnergyscanAdmin(admin.ModelAdmin):
+    list_display = ('species', 'origin_species')
+    search_fields = ('species', 'origin_species')
+    raw_id_fields = ('species', 'origin_species')
+
+admin.site.register(Species, SpeciesAdmin)
 admin.site.register(Resonance)
-admin.site.register(Source)
+admin.site.register(Source, SourceAdmin)
 admin.site.register(Massspec)
-admin.site.register(Energyscan)
+admin.site.register(Energyscan, EnergyscanAdmin)
 admin.site.register(Author)
 admin.site.register(Experiment)
