8cdaff53ca4d6c8760b0b6ac59103240a31aadbb
johannespostler <johannes.postler@uibk.ac.at>
2012-12-07 10:41:52 +0100

    Fix bug in search function. Provide admin action to create new species based on existing one.

--- nodes/IDEADB/node/admin.py
+++ nodes/IDEADB/node/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 #import your models here like this:
 from IDEADB.node.models import *
@@ -7,14 +9,23 @@ class SpeciesAdmin(admin.ModelAdmin):
     list_display = ('chemical_formula', 'name', 'mass')
     search_fields = ('chemical_formula', 'name', 'mass')
 
+    actions = ['create_new_species_based_on_existing_one']
+
+    def create_new_species_based_on_existing_one(self, request, queryset):
+        if len(queryset) == 1:
+            s = queryset.get()
+            return HttpResponseRedirect('add/?chemical_formula=%s&mass=%s&nuclear_charge=%s' % (s.chemical_formula, s.mass, s.nuclear_charge))
+        else:
+            messages.error(request, 'You can only base a new species on one existing species, stupid.')
+
 class SourceAdmin(admin.ModelAdmin):
     list_display = ('title', 'journal', 'year')
-    search_fields = ('title', 'journal', 'year')
+    search_fields = ('title', 'journal', 'year', 'authors__lastname', 'authors__firstname')
     filter_horizontal = ('authors',)
 
 class EnergyscanAdmin(admin.ModelAdmin):
     list_display = ('species', 'origin_species')
-    search_fields = ('species', 'origin_species')
+    search_fields = ('species__chemical_formula', 'species__name', 'origin_species__chemical_formula', 'origin_species__name')
     raw_id_fields = ('species', 'origin_species')
 
 admin.site.register(Species, SpeciesAdmin)
