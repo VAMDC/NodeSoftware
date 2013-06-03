cb2fd167b379d5d0cd415574165a28a5e3ea359f
Thomas Marquart <thomas@marquart.se>
2013-06-03 15:41:35 +0200

    add defalt log filter to settings_default.py to get rid of deprecation warning

--- nodes/settings_default.py
+++ nodes/settings_default.py
@@ -155,6 +155,11 @@ LOGGING = {
             'format': '%(asctime)s %(levelname)s %(message)s'
         },
     },
+    'filters': {
+        'require_debug_false': {
+        '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'null': {
             'level':'DEBUG',
@@ -166,6 +171,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler',
             'include_html': True,
         },
