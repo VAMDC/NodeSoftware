96173a9e05deb8ab63f3267900a0535c4e60da24
Thomas Marquart <thomas.marquart@fysast.uu.se>
2017-01-30 14:02:26 +0100

    split the wsgi files for dev node and main, specify the actual settings file in them, not a symlink

--- nodes/vald/wsgi_dev.py
+++ nodes/vald/wsgi_dev.py
@@ -1,6 +1,6 @@
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_atom")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_dev")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
