f4acbf3dc1b8d6158047127f50cab9b0d9d07f09
Johannes Postler <johannes.postler@uibk.ac.at>
2017-03-14 16:26:40 +0100

    Adapt IDEADB for django 1.10

--- nodes/IDEADB/node/urls.py
+++ nodes/IDEADB/node/urls.py
@@ -2,15 +2,15 @@
 # Use this file to connect views from views.py in the same
 # directory to their URLs.
 
-from django.conf.urls import patters, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 
 #admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+]
 
 #urlpatterns = patterns(settings.NODENAME+'.node.views',
 #                       (r'^$', 'index'),
