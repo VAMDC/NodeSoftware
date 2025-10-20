73ccc03e717b1806b7a4ac075192ce817ab498d3
Thomas Marquart <thomas@marquart.se>
2025-10-20 10:41:59 +0200

    add back the states count for HEAD reqs, it was a missing index that made things slow, not this.

--- nodes/vald/node_atom/queryfunc.py
+++ nodes/vald/node_atom/queryfunc.py
@@ -19,10 +19,9 @@ def returnHeaders(transs):
         headers['COUNT-ATOMS'] = \
             len(transs.values_list('species_id',flat=True).distinct())
         headers['COUNT-SPECIES'] = headers['COUNT-ATOMS']
-        # Skip state counting - it's expensive and not critical for HEAD requests
-        # sids = transs.values_list('upstate_id','lostate_id')
-        # sids = set(item for s in sids for item in s)
-        # headers['COUNT-STATES'] = len(sids)
+        sids = transs.values_list('upstate_id','lostate_id')
+        sids = set(item for s in sids for item in s)
+        headers['COUNT-STATES'] = len(sids)
 
     return headers
 
