a08fb705852ed71919f08fca10eefea79ee2ff97
Thomas Marquart <thomas@marquart.se>
2025-11-10 13:59:32 +0100

    add settings_molec.py

--- nodes/vald/settings_molec.py
+++ nodes/vald/settings_molec.py
@@ -1,27 +1,54 @@
 from settings_default import *
 
-DEBUG = True
-#DEBUG = False
-TRANSLIM = 6000
+DEBUG = False
+#DEBUG = True
+TRANSLIM = 200000
+QUERY_STORE_ACTIVE = False
 
-INSTALLED_APPS.remove(NODEPKG)
-INSTALLED_APPS.append(NODENAME+'.node_common')
-NODEPKG=NODENAME+'.node_molec'
-INSTALLED_APPS.append(NODEPKG)
+try:
+    INSTALLED_APPS.remove('node')
+except:
+    pass
+
+NODEPKG='node_molec'
+if not 'node_common' in INSTALLED_APPS:
+    INSTALLED_APPS.append('node_common')
+if not NODEPKG in INSTALLED_APPS:
+    INSTALLED_APPS.append(NODEPKG)
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'vald_molec',
-    'USER': 'vald',
-    'PASSWORD': 'V@ld',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': 'vald_molec.sqlite',
   },
 }
+LAST_MODIFIED = datetime.date(2025,10,22)
+NODEVERSION = LAST_MODIFIED.isoformat()
 
-ADMINS = (('Thomas', 'thomas@marquart.se'),)
-EXAMPLE_QUERIES = []
-SERVER_EMAIL = 'vamdc@vald.astro.uu.se'
+EXAMPLE_QUERIES = [\
+    "SELECT ALL WHERE RadTransWavelength > 4000 AND RadTransWavelength < 4000.01",
+    "SELECT SPECIES",
+    ]
+ADMINS = (('Thomas', 'thomas.marquart@astro.uu.se'),)
+SERVER_EMAIL = 'thomas.marquart.astro.uu.se'
+DEPLOY_URL = 'http://localhost:8000/tap/'
+STATIC_URL = '/static/'
 
-LOGGING['handlers']['logfile']['filename'] = '/tmp/molecnode.log'
+VAMDC_APPS = [\
+    "ivo://vamdc/xsams-mux",
+    "ivo://vamdc/xsams2sme",
+    "ivo://vamdc/XSAMS-bibtex",
+    "ivo://vamdc/xsams-views",
+    ]
+
+LOGGING['handlers']['logfile']['filename'] = 'molec.log'
 if not DEBUG:
     LOGGING['handlers']['logfile']['level'] = 'INFO'
+
+
+#LOGGING['loggers']['django.db.backends'] = {
+#      'handlers': ['console', 'logfile'],
+#      'level': 'DEBUG',
+#      'propagate': False,
+#  }
+
